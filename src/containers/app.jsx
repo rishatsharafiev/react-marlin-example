9fb91ebe91ec4139ef45a5065bab0b12e8210632
Rishat Sharafiev <rishatsharafiev@ya.ru>
2018-05-13 19:41:41 +0300

    Add dependencies and set up application structure

---
+++ src/containers/app.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import Main from 'containers/layout/main'
+import Container from 'containers/layout/container'
+
+const App = () => (
+  <Main>
+    <Container />
+  </Main>
+)
+
+export default App
